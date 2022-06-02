28288c81c82bd646f0cc6d634d0bb3572f25d471
Ryan Holmes <holmes.ryan.m@gmail.com>
2022-06-03 09:11:43 +1000

    cleanup.

---
+++ processing/processing.m
@@ -0,0 +1,8 @@
+# NCO commands:
+# ncrcat -v time_bound,time,z_t,TLONG,TLAT,SALT -d z_t,0,0 v1p2p2_B1850_f19g16_conf9_BrR1000_dtcount29.SALT.030101-040012.nc ../../rmh561/mom/input/amocoff/v1p2p2_B1850_f19g16_conf9_BrR1000_dtcount29.SALT.030101-040012.nc
+#
+# ncdiff
+#
+# ncra -d time,599,1199
+
+
